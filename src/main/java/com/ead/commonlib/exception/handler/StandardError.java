49dce680cf56bb535816c389c1caa31fc6056e77
Pedro Colombano <pedromaiacolombano@gmail.com>
2023-08-09 15:44:27 -0300

    feat: exception handling for invalid field with bean validation

--- src/main/java/com/ead/commonlib/exception/handler/StandardError.java
+++ src/main/java/com/ead/commonlib/exception/handler/StandardError.java
@@ -1,6 +1,7 @@
 package com.ead.commonlib.exception.handler;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,11 +9,14 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class StandardError implements Serializable {
 
     private static final long serialVersionUID = 1678613687288142170L;
@@ -24,5 +28,8 @@ public class StandardError implements Serializable {
     private String message;
     private Integer statusCode;
     private String path;
+
+    @Builder.Default
+    private final List<FieldError> errors = new ArrayList<>();
 
 }
